8cd7c3fddee7f1afcf2042e44a1165f354796f38
DTalone <farisaziz22@gmail.com>
2020-11-24 23:07:36 +0700

    asdf

--- battle.pl
+++ battle.pl
@@ -1,6 +1,66 @@
-:- dynamic(battle/1).
-:- dynamic(kabur/0).
+:- dynamic(round/1). %tanda apabila sedang battle
+:- dynamic(kabur/0).  %tanda jika berhasil kabur
+:- dynamic(currEnemy/6). %menyimpan enemy sekarang
 
+battle(A) :- enemy(A,B,C,D,E),
+          asserta(round(0)),
+          adjusmentEnemy(A),
+          asserta(currEnemy(A,B,C,D,E,B)),
+          repeat,
+          round(Round), Roundnew is Round + 1,
+          retract(round(Round)), asserta(round(Roundnew)),
+          turnUser,
+          currEnemy(_,W,_,_,_,_),
+          (W < 0 -> write('you win'), retractall(round(_)),!;write('try again'),nl),
+          printStat,
+          turnEnemy,
+          character(_,X,_,_,_,_,_,_),
+          printStat,
+          (X < 0 -> write('you lose'), retractall(round(_)),!;write('try again'),nl, fail).
+
+turnUser :- write('Apa yang akan Anda lakukan ?'),nl,
+            write('- attack.'),nl,
+            write('- specialAttack.'),nl,
+            write('- run.'),nl,
+            write('- usePotions.'),nl,
+            read(X),X.
+
+turnEnemy :- character(A,B,C,D,E,F,G,H),
+            currEnemy(_,_,K,_,_,_),
+            Bnew is B - K + D,
+            retract(character(A,B,C,D,E,F,G,H)),
+            asserta(character(A,Bnew,C,D,E,F,G,H)),
+            write('awww enemy menyerang'), nl.
+
+printStat :- character(A,B,_,_,_,_,_,H),currEnemy(I,J,_,_,_,K),
+          write('Job      : '), write(A),write('          '),write('Type     : '), write(I),nl,
+          write('HP       : '), write(B),write('/'),write(H),write('          '),write('HP       : '), write(J),write('/'),write(K),nl.
+
+attack :- character(_,_,X,_,_,_,_,_),
+          currEnemy(A,B,C,D,E,F),
+          Bnew is B - X + D,
+          retract(currEnemy(A,B,C,D,E,F)),
+          asserta(currEnemy(A,Bnew,C,D,E,F)),
+          write('serangan berhasil'),nl.
+%
+% attack :-
+%
+%
+% attack :-
+%
+% specialAttack :-
+%
+% specialAttack :-
+%
+%
+% specialAttack :-
+%
+% usePotions :-
+%
+% usePotions :-
+%
+% usePotions :-
+%
 
 run :-
     gameOver, lose, !.
--- enemy.pl
+++ enemy.pl
@@ -1,3 +1,56 @@
 /* File : enemy.pl */
 
-:-dynamic(enemy/7).
+:-dynamic(enemy/5).
+
+%enemy(A,B,C,D,E)
+%enemy(type, hp, attack, defense, level).
+% character(A,B,C,D,E,F,G,H)
+% character(Jenis, HP, attack, defense, level, exp, base exp, max HP)
+initEnemy :-asserta(enemy(slime, 250,70,10,1)),
+            asserta(enemy(goblin, 150,100,50,1)),
+            asserta(enemy(wolf, 300,150,70,1)),
+            asserta(enemy(golem, 400,90,150,1)),
+            asserta(enemy(wizard, 450,200,100,1)).
+statusEnemy :- enemy(A,B,C,D,E),
+          write('Type     : '), write(A),nl,
+          write('HP       : '), write(B),nl,
+          write('Attack   : '), write(C),nl,
+          write('Defense  : '), write(D),nl,
+          write('Level    : '), write(E),nl.
+
+adjusmentEnemy(X):- X = slime, character(_,_,_,_,E,_,_,_),
+            enemy(X, F,G,H,I),
+            Ben is F + 70 * (E - I),
+            Cen is G + 50 * (E - I),
+            Den is H + 30 * (E - I),
+            retract(enemy(X,F,G,H,I)),
+            asserta(enemy(X, Ben,Cen,Den,I)),!.
+
+adjusmentEnemy(X):- X = goblin, character(_,_,_,_,E,_,_,_),
+            enemy(X, F,G,H,I),
+            Ben is F + 100 * (E - I),
+            Cen is G + 70 * (E - I),
+            Den is H + 40 * (E - I),
+            retract(enemy(X,F,G,H,I)),
+            asserta(enemy(X, Ben,Cen,Den,I)),!.
+adjusmentEnemy(X):- X = wolf, character(_,_,_,_,E,_,_,_),
+            enemy(X, F,G,H,I),
+            Ben is F + 130 * (E - I),
+            Cen is G + 90 * (E - I),
+            Den is H + 50 * (E - I),
+            retract(enemy(X,F,G,H,I)),
+            asserta(enemy(X, Ben,Cen,Den,I)),!.
+adjusmentEnemy(X):- X = golem, character(_,_,_,_,E,_,_,_),
+            enemy(X, F,G,H,I),
+            Ben is F + 160 * (E - I),
+            Cen is G + 100 * (E - I),
+            Den is H + 60 * (E - I),
+            retract(enemy(X,F,G,H,I)),
+            asserta(enemy(X, Ben,Cen,Den,I)),!.
+adjusmentEnemy(X):- X = wizard, character(_,_,_,_,E,_,_,_),
+            enemy(X, F,G,H,I),
+            Ben is F + 190 * (E - I),
+            Cen is G + 120 * (E - I),
+            Den is H + 70 * (E - I),
+            retract(enemy(X,F,G,H,I)),
+            asserta(enemy(X, Ben,Cen,Den,I)),!.
