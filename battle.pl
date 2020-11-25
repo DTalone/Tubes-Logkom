aac3710e05b405774f87d8ca8fdec25071587eea
DTalone <farisaziz22@gmail.com>
2020-11-25 10:27:58 +0700

    tes

--- battle.pl
+++ battle.pl
@@ -33,16 +33,21 @@ turnEnemy :- character(A,B,C,D,E,F,G,H),
 
 printStat :- character(A,B,_,_,_,_,_,H),currEnemy(I,J,_,_,_,K), round(Round),
           write('Giliran ke-'),write(Round),nl,
-          write('Job      : '), write(A),write('          '),write('  Type       : '), write(I),nl,
+          write('Job      : '), write(A),write('          '),write('Type       : '), write(I),nl,
           write('HP       : '), write(B),write('/'),write(H),write('          '),write('HP       : '), write(J),write('/'),write(K),nl.
 
 attack :- character(_,_,X,_,_,_,_,_),
           currEnemy(A,B,C,D,E,F),
-          Bnew is B - X + D, (Bnew > B -> Bnew is B - X),
+          Bnew is B - X + D, Bnew < B,
           retract(currEnemy(A,B,C,D,E,F)),
           asserta(currEnemy(A,Bnew,C,D,E,F)),
           write('serangan berhasil'),nl.
-
+attack :- character(_,_,X,_,_,_,_,_),
+          currEnemy(A,B,C,D,E,F),
+          Bnew is B - X, Bnew > B,
+          retract(currEnemy(A,B,C,D,E,F)),
+          asserta(currEnemy(A,Bnew,C,D,E,F)),
+          write('serangan berhasil'),nl.
 specialAttack :- round(X),Z is X mod 3, (Z =:= 0-> write('mematikan'),currEnemy(A,B,C,D,E,F),
                   Bnew is B - 2 * X,retract(currEnemy(A,B,C,D,E,F)),asserta(currEnemy(A,Bnew,C,D,E,F)),nl;
                   round(Round), Roundnew is Round - 1,retract(round(Round)), asserta(round(Roundnew)),fail).
